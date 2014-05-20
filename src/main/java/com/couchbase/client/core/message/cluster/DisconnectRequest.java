22de2074ab5ab04ca9c8e01462190d7b65347a3d
Michael Nitschinger <michael@nitschinger.at>
2014-05-20 09:47:12 +0200

    docblock change.
    Change-Id: Ia545840c63ca3d89aa148bbd81a5b9b5ee95c1df

--- src/main/java/com/couchbase/client/core/message/cluster/DisconnectRequest.java
+++ src/main/java/com/couchbase/client/core/message/cluster/DisconnectRequest.java
@@ -2,6 +2,12 @@ package com.couchbase.client.core.message.cluster;
 
 import com.couchbase.client.core.message.AbstractCouchbaseRequest;
 
+/**
+ * Disconnects all open buckets.
+ *
+ * @author Michael Nitschinger
+ * @since 1.0
+ */
 public class DisconnectRequest extends AbstractCouchbaseRequest implements ClusterRequest {
     public DisconnectRequest() {
         super(null, null);
