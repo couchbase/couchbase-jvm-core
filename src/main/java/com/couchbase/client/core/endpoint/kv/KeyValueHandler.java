53fbb5f5cc0a92ef881fc70dc5d8ad70687f864b
Michael Nitschinger <michael@nitschinger.at>
2014-12-17 15:03:49 +0100

    JVMCBC-75: Only extract response bytes when success from observe and counter.
    Motivation
    ----------
    During non-success responses, including not my vbucket responses, data cannot be
    extracted (since it looks different) and therefore the status needs to be taken
    into account.
    
    This also prevents observe from working correctly during rebalance with higher
    load (the chance is higher to hit a NMVB during observe calls).
    
    Modifications
    -------------
    Make sure that when non-success responses are returned from operations, they are safely
    set to a default value (the higher level takes care of either retrying transparently
    or failing), but no exception (like index out of bounds) is raised.
    
    Result
    ------
    More resiliency during non-success responses, including rebalance phases.
    
    Change-Id: Ida290e9b9196cd6aeed4b0a6f0edc5987b599e62
    Reviewed-on: http://review.couchbase.org/44422
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>
    Reviewed-on: http://review.couchbase.org/44688

--- src/main/java/com/couchbase/client/core/endpoint/kv/KeyValueHandler.java
+++ src/main/java/com/couchbase/client/core/endpoint/kv/KeyValueHandler.java
@@ -99,7 +99,7 @@ public class KeyValueHandler
     /**
      * Represents the "Not My VBucket" status response.
      */
-    private static final byte STATUS_NOT_MY_VBUCKET = 0x07;
+    public static final byte STATUS_NOT_MY_VBUCKET = 0x07;
 
     /**
      * Creates a new {@link KeyValueHandler} with the default queue for requests.
@@ -432,13 +432,18 @@ public class KeyValueHandler
         } else if (request instanceof RemoveRequest) {
             response = new RemoveResponse(status, cas, bucket, content, request);
         } else if (request instanceof CounterRequest) {
-            response = new CounterResponse(status, bucket, msg.content().readLong(), cas, request);
+            long value = status.isSuccess() ? msg.content().readLong() : 0;
+            if (msg.content() != null) {
+                msg.content().release();
+            }
+            response = new CounterResponse(status, bucket, value, cas, request);
         } else if (request instanceof UnlockRequest) {
             response = new UnlockResponse(status, bucket, content, request);
         } else if (request instanceof TouchRequest) {
             response = new TouchResponse(status, bucket, content, request);
         } else if (request instanceof ObserveRequest) {
-            byte observed = content.getByte(content.getShort(2) + 4);
+            byte observed = status.isSuccess()
+                ? content.getByte(content.getShort(2) + 4) : ObserveResponse.ObserveStatus.UNKNOWN.value();
             response = new ObserveResponse(status, observed, ((ObserveRequest) request).master(), bucket,
                 content, request);
         } else if (request instanceof AppendRequest) {
