f6d35f8f26a00f45f551c1ae2e84750dd522cc06
Michael Nitschinger <michael@nitschinger.at>
2014-12-30 15:50:19 +0100

    JVMCBC-104: CouchbaseNodes need to be equals on same InetAddr
    Motivation
    ----------
    The RequestHandler stores all the CouchbaseNodes in a Set. Even
    when race conditions happen, we need to make sure that the list
    of nodes never contains duplicates.
    
    Modifications
    -------------
    The CouchbaseNode now has custom hashCode and equals methods which
    align on the stored InetAddress. That way, nodes pointing to the
    same address are treated the same and never stored together in the
    set.
    
    Result
    ------
    More consistent handling of nodes, and never duplicates in the
    node list set.
    
    Change-Id: I23cbdd72f655870777bc3476e779da8c0822f40d
    Reviewed-on: http://review.couchbase.org/44763
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>

--- src/main/java/com/couchbase/client/core/node/CouchbaseNode.java
+++ src/main/java/com/couchbase/client/core/node/CouchbaseNode.java
@@ -296,4 +296,21 @@ public class CouchbaseNode extends AbstractStateMachine<LifecycleState> implemen
     protected static String logIdent(final InetAddress hostname) {
         return "[" + hostname.getHostName() + "]: ";
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CouchbaseNode that = (CouchbaseNode) o;
+
+        if (!hostname.equals(that.hostname)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return hostname.hashCode();
+    }
 }
