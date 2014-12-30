2898dc373bfbf34d529f51fb3e440383d4c43082
Michael Nitschinger <michael@nitschinger.at>
2014-12-30 08:56:36 +0100

    JVMCBC-98: Fall back to verbose config if terse returns 404
    Motivation
    ----------
    When the terse bucket config over HTTP is not accessible (which is the
    case on older server versions), the code needs to fall back to the
    verbose config when it both fails or just returns a 404 not found.
    
    Modifications
    -------------
    The code now properly checks if the terse config is a success and if not,
    converts it into an exception that will be caught by the next handler.
    Note that while this may not be the most performant way to handle this,
    the code path is not travelled much and makes error handling more uniform.
    
    Also, a test case has been added and another one corrected (the wrong call
    was used and overriden by the success message, leading to an always
    succeeding test).
    
    Result
    ------
    Correct fallback handling on older server versions.
    
    Change-Id: I1e61ff30599dbac153da4a65d15334683918436b
    Reviewed-on: http://review.couchbase.org/44748
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>
    Reviewed-on: http://review.couchbase.org/44768

--- src/test/java/com/couchbase/client/core/config/refresher/HttpRefresherTest.java
+++ src/test/java/com/couchbase/client/core/config/refresher/HttpRefresherTest.java
@@ -93,8 +93,42 @@ public class HttpRefresherTest {
         Observable<CouchbaseResponse> successResponse = Observable.just((CouchbaseResponse)
                 new BucketStreamingResponse(configStream, "", ResponseStatus.SUCCESS, null)
         );
-        when(cluster.send(isA(BucketStreamingRequest.class))).thenReturn(failingResponse);
-        when(cluster.send(isA(BucketStreamingRequest.class))).thenReturn(successResponse);
+        when(cluster.send(isA(BucketStreamingRequest.class))).thenReturn(failingResponse, successResponse);
+
+        HttpRefresher refresher = new HttpRefresher(cluster);
+
+        final CountDownLatch latch = new CountDownLatch(3);
+        refresher.configs().subscribe(new Action1<BucketConfig>() {
+            @Override
+            public void call(BucketConfig bucketConfig) {
+                assertEquals("default", bucketConfig.name());
+                latch.countDown();
+            }
+        });
+
+        Observable<Boolean> observable = refresher.registerBucket("default", "");
+        assertTrue(observable.toBlocking().single());
+        assertTrue(latch.await(3, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void shouldFallbackToVerboseIfTerseIsNotSuccess() throws Exception {
+        ClusterFacade cluster = mock(ClusterFacade.class);
+
+        Observable<String> configStream = Observable.just(
+            Resources.read("stream1.json", this.getClass()),
+            Resources.read("stream2.json", this.getClass()),
+            Resources.read("stream3.json", this.getClass())
+        );
+
+        Observable<CouchbaseResponse> failingResponse = Observable.just((CouchbaseResponse)
+                new BucketStreamingResponse(null, "", ResponseStatus.NOT_EXISTS, null)
+        );
+        Observable<CouchbaseResponse> successResponse = Observable.just((CouchbaseResponse)
+                new BucketStreamingResponse(configStream, "", ResponseStatus.SUCCESS, null)
+        );
+
+        when(cluster.send(isA(BucketStreamingRequest.class))).thenReturn(failingResponse, successResponse);
 
         HttpRefresher refresher = new HttpRefresher(cluster);
 
