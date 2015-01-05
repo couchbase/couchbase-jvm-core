b915a1355dfc936cbfce92cd2a869d887d8692b4
Michael Nitschinger <michael@nitschinger.at>
2015-01-05 14:49:59 +0100

    JVMCBC-116: Do not route view requests to nodes without partitions.
    Motivation
    ----------
    When a node is removed from the cluster, there is a timeframe when all partitions went away,
    but it is still part of the logical cluster config. At this point in time, view requests
    should not be routed to this node anymore.
    
    Modifications
    -------------
    The changesets has three components:
    
    	1) It provides a utility method on the CouchbaseBucketConfig that returns a boolean
    	   to quickly check if the given node contains at least one primary partition.
    
    	   Note that these are cached when the config is created to save CPU cycled on the
    	   hot code path (when checking).
    
    	2) It adds a check to the ViewLocator so that a node is not used when it has no
    	   partitions assigned to it.
    
    	3) Relatedly, the code in the ViewLocator immediately rejects requests that
    	   cannot be served from the config type. This mainly includes view requests
    	   being cancelled right away against a memcached bucket type.
    
    	   As part of this effort, a new exception has been added to semantically
    	   indicate this state.
    
    Tests have been added to verify the new functionality on a unit-test basis.
    
    Result
    ------
    View requests are not routed to nodes anymore which do not have primary partitions
    assigned to it.
    
    Change-Id: I02b92f7fa2cf379e311eb88f01a87336b746529a
    Reviewed-on: http://review.couchbase.org/44898
    Reviewed-by: Simon Baslé <simon@couchbase.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/test/java/com/couchbase/client/core/node/locate/ViewLocatorTest.java
+++ src/test/java/com/couchbase/client/core/node/locate/ViewLocatorTest.java
@@ -21,18 +21,32 @@
  */
 package com.couchbase.client.core.node.locate;
 
+import com.couchbase.client.core.ServiceNotAvailableException;
 import com.couchbase.client.core.config.ClusterConfig;
+import com.couchbase.client.core.config.CouchbaseBucketConfig;
+import com.couchbase.client.core.config.MemcachedBucketConfig;
+import com.couchbase.client.core.message.CouchbaseRequest;
+import com.couchbase.client.core.message.CouchbaseResponse;
 import com.couchbase.client.core.message.view.ViewQueryRequest;
 import com.couchbase.client.core.node.Node;
 import org.junit.Test;
-
+import rx.observers.TestSubscriber;
+import rx.subjects.AsyncSubject;
+import rx.subjects.Subject;
 import java.net.InetAddress;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -49,7 +63,11 @@ public class ViewLocatorTest {
         Locator locator = new ViewLocator();
 
         ViewQueryRequest request = mock(ViewQueryRequest.class);
+        when(request.bucket()).thenReturn("default");
         ClusterConfig configMock = mock(ClusterConfig.class);
+        CouchbaseBucketConfig bucketConfigMock = mock(CouchbaseBucketConfig.class);
+        when(bucketConfigMock.hasPrimaryPartitionsOnNode(any(InetAddress.class))).thenReturn(true);
+        when(configMock.bucketConfig("default")).thenReturn(bucketConfigMock);
         Set<Node> nodes = new HashSet<Node>();
         Node node1Mock = mock(Node.class);
         when(node1Mock.hostname()).thenReturn(InetAddress.getByName("192.168.56.101"));
@@ -71,6 +89,63 @@ public class ViewLocatorTest {
 
         assertEquals(foundFirst, foundLast);
         assertNotEquals(foundFirst, foundSecond);
+    }
+
+    @Test
+    public void shouldSkipNodeWithoutPartition() throws Exception {
+        Locator locator = new ViewLocator();
+
+        ViewQueryRequest request = mock(ViewQueryRequest.class);
+        when(request.bucket()).thenReturn("default");
+        ClusterConfig configMock = mock(ClusterConfig.class);
+        CouchbaseBucketConfig bucketConfigMock = mock(CouchbaseBucketConfig.class);
+        when(bucketConfigMock.hasPrimaryPartitionsOnNode(InetAddress.getByName("192.168.56.101"))).thenReturn(false);
+        when(bucketConfigMock.hasPrimaryPartitionsOnNode(InetAddress.getByName("192.168.56.102"))).thenReturn(true);
+
+        when(configMock.bucketConfig("default")).thenReturn(bucketConfigMock);
+        Set<Node> nodes = new LinkedHashSet<Node>();
+        Node node1Mock = mock(Node.class);
+        when(node1Mock.hostname()).thenReturn(InetAddress.getByName("192.168.56.101"));
+        Node node2Mock = mock(Node.class);
+        when(node2Mock.hostname()).thenReturn(InetAddress.getByName("192.168.56.102"));
+        nodes.addAll(Arrays.asList(node1Mock, node2Mock));
+
+        Node[] located = locator.locate(request, nodes, configMock);
+        assertEquals(0, located.length);
+
+        located = locator.locate(request, nodes, configMock);
+        assertEquals(1, located.length);
+        InetAddress foundSecond = located[0].hostname();
+
+        located = locator.locate(request, nodes, configMock);
+        assertEquals(0, located.length);
+
+        assertEquals(foundSecond, InetAddress.getByName("192.168.56.102"));
+    }
+
+    @Test
+    public void shouldFailWhenUsedAgainstMemcacheBucket() {
+        Locator locator = new ViewLocator();
+
+        ClusterConfig config = mock(ClusterConfig.class);
+        when(config.bucketConfig("default")).thenReturn(mock(MemcachedBucketConfig.class));
+
+        CouchbaseRequest request = mock(ViewQueryRequest.class);
+        Subject<CouchbaseResponse, CouchbaseResponse> response = AsyncSubject.create();
+        when(request.bucket()).thenReturn("default");
+        when(request.observable()).thenReturn(response);
+
+        TestSubscriber<CouchbaseResponse> subscriber = new TestSubscriber<CouchbaseResponse>();
+        response.subscribe(subscriber);
+
+        Node[] located = locator.locate(request, Collections.<Node>emptySet(), config);
+
+        assertNull(located);
+
+        subscriber.awaitTerminalEvent(1, TimeUnit.SECONDS);
+        List<Throwable> errors = subscriber.getOnErrorEvents();
+        assertEquals(1, errors.size());
+        assertTrue(errors.get(0) instanceof ServiceNotAvailableException);
     }
 
 }
