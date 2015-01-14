5c1cbdafa61760b0e944567c66bbe67f0f95d8b4
Sergey Avseyev <sergey.avseyev@gmail.com>
2015-01-15 01:04:32 +0300

    Preserve environment invariant for feature tests
    Motivation
    ----------
    Feature checks are sensitive for system properties. And these systems
    properties are the only way to adjust environment in cluster-dependent
    tests as DCPMessageTest, which are not enabled by default, because the
    server might lack of feature support.
    
    But in case of tests, which verify the actual feature checking, should
    work in the same way whether or not the system properties set.
    
    Modification
    ------------
    Reset system properties enabling Query and DCP features before executing
    the tests, and restore on exit.
    
    Result
    ------
    Feature checking tests more robust
    
    Change-Id: I97fe3f2a4c009323d9a52314fd502dc244281117
    Reviewed-on: http://review.couchbase.org/45346
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/test/java/com/couchbase/client/core/RequestHandlerTest.java
+++ src/test/java/com/couchbase/client/core/RequestHandlerTest.java
@@ -165,30 +165,60 @@ public class RequestHandlerTest {
 
     @Test
     public void shouldPreventFeatureDependentRequestsWhenFeatureDisabled() {
-        QueryRequest mockQueryRequest = mock(QueryRequest.class);
-        DCPRequest mockDcpRequest = mock(DCPRequest.class);
-        CouchbaseRequest mockKeyValueRequest = mock(GetRequest.class);
-        CoreEnvironment env = DefaultCoreEnvironment.create();
-        RequestHandler handler = new DummyLocatorClusterNodeHandler(env);
+        String dcpWasEnabled = System.getProperty("com.couchbase.dcpEnabled");
+        String queryWasEnabled = System.getProperty("com.couchbase.queryEnabled");
+        try {
+            System.getProperties().remove("com.couchbase.dcpEnabled");
+            System.getProperties().remove("com.couchbase.queryEnabled");
+            QueryRequest mockQueryRequest = mock(QueryRequest.class);
+            DCPRequest mockDcpRequest = mock(DCPRequest.class);
+            CouchbaseRequest mockKeyValueRequest = mock(GetRequest.class);
+            CoreEnvironment env = DefaultCoreEnvironment.builder()
+                    .queryEnabled(false)
+                    .dcpEnabled(false)
+                    .build();
+            RequestHandler handler = new DummyLocatorClusterNodeHandler(env);
 
-        assertFeatureForRequest(handler, mockQueryRequest, false);
-        assertFeatureForRequest(handler, mockDcpRequest, false);
-        assertFeatureForRequest(handler, mockKeyValueRequest, true);
+            assertFeatureForRequest(handler, mockQueryRequest, false);
+            assertFeatureForRequest(handler, mockDcpRequest, false);
+            assertFeatureForRequest(handler, mockKeyValueRequest, true);
+        } finally {
+            if (dcpWasEnabled != null) {
+                System.setProperty("com.couchbase.dcpEnabled", dcpWasEnabled);
+            }
+            if (queryWasEnabled != null) {
+                System.setProperty("com.couchbase.queryEnabled", queryWasEnabled);
+            }
+        }
     }
 
     @Test
     public void shouldAllowQueryWhenQueryFeatureEnabled() {
-        QueryRequest mockQueryRequest = mock(QueryRequest.class);
-        DCPRequest mockDcpRequest = mock(DCPRequest.class);
-        CouchbaseRequest mockKeyValueRequest = mock(GetRequest.class);
-        CoreEnvironment env = DefaultCoreEnvironment.builder()
-                .queryEnabled(true)
-                .build();
-        RequestHandler handler = new DummyLocatorClusterNodeHandler(env);
+        String dcpWasEnabled = System.getProperty("com.couchbase.dcpEnabled");
+        String queryWasEnabled = System.getProperty("com.couchbase.queryEnabled");
+        try {
+            System.getProperties().remove("com.couchbase.dcpEnabled");
+            System.getProperties().remove("com.couchbase.queryEnabled");
+            QueryRequest mockQueryRequest = mock(QueryRequest.class);
+            DCPRequest mockDcpRequest = mock(DCPRequest.class);
+            CouchbaseRequest mockKeyValueRequest = mock(GetRequest.class);
+            CoreEnvironment env = DefaultCoreEnvironment.builder()
+                    .queryEnabled(true)
+                    .dcpEnabled(true)
+                    .build();
+            RequestHandler handler = new DummyLocatorClusterNodeHandler(env);
 
-        assertFeatureForRequest(handler, mockQueryRequest, true);
-        assertFeatureForRequest(handler, mockDcpRequest, false);
-        assertFeatureForRequest(handler, mockKeyValueRequest, true);
+            assertFeatureForRequest(handler, mockQueryRequest, true);
+            assertFeatureForRequest(handler, mockDcpRequest, true);
+            assertFeatureForRequest(handler, mockKeyValueRequest, true);
+        } finally {
+            if (dcpWasEnabled != null) {
+                System.setProperty("com.couchbase.dcpEnabled", dcpWasEnabled);
+            }
+            if (queryWasEnabled != null) {
+                System.setProperty("com.couchbase.queryEnabled", queryWasEnabled);
+            }
+        }
     }
 
     @Test
