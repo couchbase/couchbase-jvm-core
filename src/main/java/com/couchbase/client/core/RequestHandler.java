5bc400c45cd64a594d05e7805f3c6db55f4a8742
Michael Nitschinger <michael@nitschinger.at>
2015-04-24 16:43:11 +0200

    JVMCBC-194: Avoid concurrent modifications on node removal.
    Motivation
    ----------
    During node removal, the code (since it reuses the same set) is
    subject to potential concurrent modification exceptions.
    
    Modifications
    -------------
    By copying the set before iterating it, the concurrent modification
    can be avoided.
    
    Result
    ------
    Safer code during reconfiguration.
    
    Change-Id: Ic70496e80f4c49f2d6a795ceea397ef4a0c8ac33
    Reviewed-on: http://review.couchbase.org/50184
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>
    Reviewed-on: http://review.couchbase.org/50186

--- src/main/java/com/couchbase/client/core/RequestHandler.java
+++ src/main/java/com/couchbase/client/core/RequestHandler.java
@@ -368,7 +368,7 @@ public class RequestHandler implements EventHandler<RequestEvent> {
                 return Observable.just(config);
             }
 
-            return Observable.from(nodes).doOnNext(new Action1<Node>() {
+            return Observable.from(new HashSet<Node>(nodes)).doOnNext(new Action1<Node>() {
                 @Override
                 public void call(Node node) {
                     removeNode(node);
