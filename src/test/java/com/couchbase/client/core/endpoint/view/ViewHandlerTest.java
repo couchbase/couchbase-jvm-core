5a02e0f3966e1d8088aeb802142848a8be313de0
Simon Baslé <simon.basle@gmail.com>
2015-05-07 18:02:38 +0200

    JVMCBC-197: have keys passed in separately for ViewQuery
    Motivation
    ----------
    Recently, a change was made so that the core switches to POST when it
    detects that a View query is too large. Body of the post is the keys
    parameters (the only one allowed, and also the most probable culprit for
    size), extracted for url and set as JSON.
    Only problem is that the client url-encodes the keys, which isn't valid
    when contained in JSON, so it fails in production.
    
    Modifications
    -------------
    The ViewQueryRequest was changed to force getting the keys array as a
    separate JSON string, not part of the "query" url.
    
    ViewHandler now uses this separate info to decide if a switch to POST
    should be made (in which case it injects the keys String into the JSON
    object of the body) or if GET is still doable (in which case it encodes
    the keys array via URLEncoder).
    
    Added tests to validate this behavior and the ways the URL is built from
    both query() and keys() (in the case of the GET).
    
    Results
    -------
    Keys are now properly encoded by the core directly and work either in a
    GET or a PUT.
    No more JSON syntax error when querying a view with a large set of keys.
    
    Change-Id: I7cd8b12c070947201483d69d32da9cdd6a44a9d1
    Reviewed-on: http://review.couchbase.org/50706
    Tested-by: Simon Baslé <simon@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/test/java/com/couchbase/client/core/endpoint/view/ViewHandlerTest.java
+++ src/test/java/com/couchbase/client/core/endpoint/view/ViewHandlerTest.java
@@ -179,7 +179,7 @@ public class ViewHandlerTest {
 
     @Test
     public void shouldEncodeViewQueryRequest() {
-        ViewQueryRequest request = new ViewQueryRequest("design", "view", true, "query", "bucket", "password");
+        ViewQueryRequest request = new ViewQueryRequest("design", "view", true, "query", null, "bucket", "password");
 
         channel.writeOutbound(request);
         HttpRequest outbound = (HttpRequest) channel.readOutbound();
@@ -389,11 +389,12 @@ public class ViewHandlerTest {
     @Test
     public void shouldEncodeLongViewQueryRequestWithPOST() {
         String keys = Resources.read("key_many.json", this.getClass());
-        String query = "stale=false&keys=" + keys + "&endKey=test";
-        ViewQueryRequest request = new ViewQueryRequest("design", "view", true, query , "bucket", "password");
+        String query = "stale=false&endKey=test";
+        ViewQueryRequest request = new ViewQueryRequest("design", "view", true, query, keys, "bucket", "password");
         channel.writeOutbound(request);
         DefaultFullHttpRequest outbound = (DefaultFullHttpRequest) channel.readOutbound();
         assertEquals(HttpMethod.POST, outbound.getMethod());
+        assertFalse(outbound.getUri().contains("keys="));
         assertTrue(outbound.getUri().endsWith("?stale=false&endKey=test"));
         String content = outbound.content().toString(CharsetUtil.UTF_8);
         assertTrue(content.startsWith("{\"keys\":["));
@@ -401,65 +402,62 @@ public class ViewHandlerTest {
     }
 
     @Test
-    public void shouldSplitOnlyKeys() {
-        String query = "keys=[1,2,3]";
-        Tuple2<String, String> split = handler.extractKeysFromQueryString(query, 4); //sure to trigger splitting
-
-        assertNotNull(split);
-        assertNotNull(split.value1());
-        assertNotNull(split.value2());
-        assertEquals(0, split.value1().length());
-        assertEquals("{\"keys\":[1,2,3]}", split.value2());
-    }
-
-    @Test
-    public void shouldSplitNoKeys() {
+    public void shouldUrlEncodeShortKeys() {
+        String urlEncodedKeys = "%5B%221%22%2C%222%22%2C%223%22%5D";
+        String keys = "[\"1\",\"2\",\"3\"]";
         String query = "stale=false&endKey=test";
-        Tuple2<String, String> split = handler.extractKeysFromQueryString(query, 4); //sure to trigger splitting
-
-        assertNotNull(split);
-        assertNotNull(split.value1());
-        assertNotNull(split.value2());
-        assertEquals(query, split.value1());
-        assertEquals(0, split.value2().length());
+        ViewQueryRequest request = new ViewQueryRequest("design", "view", true, query, keys, "bucket", "password");
+        channel.writeOutbound(request);
+        DefaultFullHttpRequest outbound = (DefaultFullHttpRequest) channel.readOutbound();
+        String failMsg = outbound.getUri();
+        assertEquals(HttpMethod.GET, outbound.getMethod());
+        assertTrue(failMsg, outbound.getUri().contains("keys="));
+        assertTrue(failMsg, outbound.getUri().endsWith("?stale=false&endKey=test&keys=" + urlEncodedKeys));
+        String content = outbound.content().toString(CharsetUtil.UTF_8);
+        assertTrue(content.isEmpty());
     }
 
     @Test
-    public void shouldSplitAndReconstructParameters() {
-        String query = "stale=false&endKey=test&keys=[1,2,3]";
-        Tuple2<String, String> split = handler.extractKeysFromQueryString(query, 4); //sure to trigger splitting
-        assertNotNull(split);
-        assertNotNull(split.value1());
-        assertNotNull(split.value2());
-        assertEquals("stale=false&endKey=test", split.value1());
-        assertEquals("{\"keys\":[1,2,3]}", split.value2());
-
-
-        query = "keys=[1,2,3]&stale=false&endKey=test";
-        split = handler.extractKeysFromQueryString(query, 4); //sure to trigger splitting
-        assertNotNull(split);
-        assertNotNull(split.value1());
-        assertNotNull(split.value2());
-        assertEquals("stale=false&endKey=test", split.value1());
-        assertEquals("{\"keys\":[1,2,3]}", split.value2());
-
-        query = "stale=false&keys=[1,2,3]&endKey=test";
-        split = handler.extractKeysFromQueryString(query, 4); //sure to trigger splitting
-        assertNotNull(split);
-        assertNotNull(split.value1());
-        assertNotNull(split.value2());
-        assertEquals("stale=false&endKey=test", split.value1());
-        assertEquals("{\"keys\":[1,2,3]}", split.value2());
+    public void shouldProduceValidUrlIfShortKeysAndNoOtherQueryParam() {
+        String urlEncodedKeys = "%5B%221%22%2C%222%22%2C%223%22%5D";
+        String keys = "[\"1\",\"2\",\"3\"]";
+        String query = "";
+        ViewQueryRequest request = new ViewQueryRequest("design", "view", true, query, keys, "bucket", "password");
+        channel.writeOutbound(request);
+        DefaultFullHttpRequest outbound = (DefaultFullHttpRequest) channel.readOutbound();
+        String failMsg = outbound.getUri();
+        assertEquals(HttpMethod.GET, outbound.getMethod());
+        assertTrue(failMsg, outbound.getUri().endsWith("?keys=" + urlEncodedKeys));
+        String content = outbound.content().toString(CharsetUtil.UTF_8);
+        assertTrue(content.isEmpty());
     }
 
     @Test
-    public void shouldNotSplitIfThresholdNotMet() {
-        String query = "stale=false&keys=[1,2,3]&endKey=test";
-        Tuple2<String, String> split = handler.extractKeysFromQueryString(query, query.length() + 1);
-        assertNotNull(split);
-        assertNotNull(split.value1());
-        assertNull(split.value2());
-        assertEquals(query, split.value1());
+    public void shouldDoNothingOnNullKeys() {
+        String keys = null;
+        String query = "stale=false&endKey=test";
+        ViewQueryRequest request = new ViewQueryRequest("design", "view", true, query, keys, "bucket", "password");
+        channel.writeOutbound(request);
+        DefaultFullHttpRequest outbound = (DefaultFullHttpRequest) channel.readOutbound();
+        assertEquals(HttpMethod.GET, outbound.getMethod());
+        assertFalse(outbound.getUri().contains("keys="));
+        assertTrue(outbound.getUri().endsWith("?stale=false&endKey=test"));
+        String content = outbound.content().toString(CharsetUtil.UTF_8);
+        assertTrue(content.isEmpty());
+    }
+
+    @Test
+    public void shouldDoNothingOnEmptyKeys() {
+        String keys = "";
+        String query = "stale=false&endKey=test";
+        ViewQueryRequest request = new ViewQueryRequest("design", "view", true, query, keys, "bucket", "password");
+        channel.writeOutbound(request);
+        DefaultFullHttpRequest outbound = (DefaultFullHttpRequest) channel.readOutbound();
+        assertEquals(HttpMethod.GET, outbound.getMethod());
+        assertFalse(outbound.getUri().contains("keys="));
+        assertTrue(outbound.getUri().endsWith("?stale=false&endKey=test"));
+        String content = outbound.content().toString(CharsetUtil.UTF_8);
+        assertTrue(content.isEmpty());
     }
 
     @Test
