b286db11598c630c385847b614fe624edc2b2d7f
Simon Baslé <simon.basle@gmail.com>
2015-05-29 11:45:14 +0200

    fix failing test related to 400 mapping
    (backported by cherry pick from 20ccde5b6325a998219a1ebd4b8367272af45d6c)
    
    see commit 85bf607c2e4b1c20842af8c7626edae1dfa2207e
    see JVMCBC-199
    
    Change-Id: I5bb766e6df46f160caf5ba50ed93cc188c304d33
    Reviewed-on: http://review.couchbase.org/51570
    Tested-by: Simon Baslé <simon@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/test/java/com/couchbase/client/core/endpoint/config/ConfigHandlerTest.java
+++ src/test/java/com/couchbase/client/core/endpoint/config/ConfigHandlerTest.java
@@ -235,7 +235,7 @@ public class ConfigHandlerTest {
         assertEquals(1, eventSink.responseEvents().size());
         FlushResponse event = (FlushResponse) eventSink.responseEvents().get(0).getMessage();
 
-        assertEquals(ResponseStatus.FAILURE, event.status());
+        assertEquals(ResponseStatus.INVALID_ARGUMENTS, event.status());
         assertEquals("{\"_\":\"Flush is disabled for the bucket\"}", event.content());
         assertTrue(requestQueue.isEmpty());
     }
