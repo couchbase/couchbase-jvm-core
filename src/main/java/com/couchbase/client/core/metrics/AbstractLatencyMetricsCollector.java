ebafbc2d75811c9e89bd42abd019b01c0729e438
Michael Nitschinger <michael@nitschinger.at>
2015-08-12 21:11:29 +0200

    JVMCBC-230: Only use one pause detector
    Motivation
    ----------
    The latency utils that we use for latency collection creates a
    pause detector per object if not set properly before. Also, it
    checks every ms and therefore creating lots of context switching.
    
    Modifications
    -------------
    To reduce context switches the code creates one pause detector
    and only wakes it up 10ms, dramatically reducing the
    context switches it creates.
    
    Result
    ------
    No more context switches than needed.
    
    Change-Id: I7791c7f04e26b388839f18600c98fc587c5e86ec
    Reviewed-on: http://review.couchbase.org/54400
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- src/main/java/com/couchbase/client/core/metrics/AbstractLatencyMetricsCollector.java
+++ src/main/java/com/couchbase/client/core/metrics/AbstractLatencyMetricsCollector.java
@@ -25,11 +25,14 @@ import com.couchbase.client.core.event.CouchbaseEvent;
 import com.couchbase.client.core.event.EventBus;
 import com.couchbase.client.core.event.metrics.LatencyMetricsEvent;
 import org.LatencyUtils.LatencyStats;
+import org.LatencyUtils.PauseDetector;
+import org.LatencyUtils.SimplePauseDetector;
 import rx.Scheduler;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The default abstract implementation for a latency metrics collector.
@@ -40,6 +43,16 @@ import java.util.concurrent.ConcurrentHashMap;
 public abstract class AbstractLatencyMetricsCollector<I extends LatencyMetricsIdentifier, E extends LatencyMetricsEvent>
     extends AbstractMetricsCollector
     implements LatencyMetricsCollector<I> {
+
+    private static final PauseDetector PAUSE_DETECTOR = new SimplePauseDetector(
+        TimeUnit.MILLISECONDS.toNanos(10),
+        TimeUnit.MILLISECONDS.toNanos(10),
+        3
+    );
+
+    static {
+        LatencyStats.setDefaultPauseDetector(PAUSE_DETECTOR);
+    }
 
     private final Map<I, LatencyStats> latencyMetrics;
     private final LatencyMetricsCollectorConfig config;
