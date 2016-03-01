411e814d99ea8a4b0c5fc8c61fa31150094b8087
Michael Nitschinger <michael@nitschinger.at>
2016-03-01 09:42:41 +0100

    Add regression test for MB-15727.
    Change-Id: I3dfffe709ac18d13013216d661704b26b0f3a7db
    Reviewed-on: http://review.couchbase.org/60690
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>

--- src/integration/java/com/couchbase/client/core/cluster/KeyValueMessageTest.java
+++ src/integration/java/com/couchbase/client/core/cluster/KeyValueMessageTest.java
@@ -22,6 +22,8 @@
 package com.couchbase.client.core.cluster;
 
 import com.couchbase.client.core.message.ResponseStatus;
+import com.couchbase.client.core.message.kv.AppendRequest;
+import com.couchbase.client.core.message.kv.AppendResponse;
 import com.couchbase.client.core.message.kv.CounterRequest;
 import com.couchbase.client.core.message.kv.CounterResponse;
 import com.couchbase.client.core.message.kv.GetRequest;
@@ -331,6 +333,36 @@ public class KeyValueMessageTest extends ClusterDependentTest {
         response = cluster().<UpsertResponse>send(request).toBlocking().single();
         ReferenceCountUtil.releaseLater(response.content());
         assertEquals(ResponseStatus.SUCCESS, response.status());
+    }
+
+    /**
+     * Verificiation for MB-15727
+     */
+    @Test
+    public void shouldGetAndLockWithAppend() {
+        String key = "get-and-lock-append";
+
+        UpsertRequest request = new UpsertRequest(key, Unpooled.copiedBuffer("foo", CharsetUtil.UTF_8), bucket());
+        UpsertResponse response = cluster().<UpsertResponse>send(request).toBlocking().single();
+        assertEquals(ResponseStatus.SUCCESS, response.status());
+        ReferenceCountUtil.releaseLater(response.content());
+
+        GetResponse getResponse = cluster().<GetResponse>send(new GetRequest(key, bucket(), true, false, 2)).toBlocking().single();
+        assertEquals(ResponseStatus.SUCCESS, getResponse.status());
+        assertEquals("foo", getResponse.content().toString(CharsetUtil.UTF_8));
+        ReferenceCountUtil.releaseLater(getResponse.content());
+
+        AppendResponse appendResponse = cluster().<AppendResponse>send(
+            new AppendRequest(key, getResponse.cas(), Unpooled.copiedBuffer("bar", CharsetUtil.UTF_8), bucket())
+        ).toBlocking().single();
+        assertEquals(ResponseStatus.SUCCESS, getResponse.status());
+        assertTrue(getResponse.cas() != appendResponse.cas());
+        ReferenceCountUtil.releaseLater(appendResponse.content());
+
+        getResponse = cluster().<GetResponse>send(new GetRequest(key, bucket(), false, false, 0)).toBlocking().single();
+        assertEquals(ResponseStatus.SUCCESS, getResponse.status());
+        assertEquals("foobar", getResponse.content().toString(CharsetUtil.UTF_8));
+        ReferenceCountUtil.releaseLater(getResponse.content());
     }
 
     @Test
