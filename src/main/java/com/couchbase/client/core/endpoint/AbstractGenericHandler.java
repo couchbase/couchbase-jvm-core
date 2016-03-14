d39454c55cdf4e8872692a437a884073ce4a3623
Michael Nitschinger <michael@nitschinger.at>
2016-03-14 08:30:16 +0100

    JVMCBC-309: Gracefully handle errors during metrics collection.
    Motivation
    ----------
    Since the underlying histogram can have bugs or we feed it invalid
    timings somehow, it can throw an exception and as a result it will
    bubble into our IO event loops.
    
    Since metrics collection is considered support functionality, it
    should under no circumstance interfer with the actual IO processing.
    
    Modifications
    -------------
    try/catch is placed around the metrics collection logic and if an
    error is detected it is logged for proper inspection.
    
    Result
    ------
    If errors happen during latency metrics collection, they are not
    affecting the actual IO workload  but are logged for proper
    supportability.
    
    Change-Id: I903f7b6d9c236c5cd3b727513bbd39cb4459a974
    Reviewed-on: http://review.couchbase.org/61413
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>
    Reviewed-by: Simon Baslé <simon@couchbase.com>

--- src/main/java/com/couchbase/client/core/endpoint/AbstractGenericHandler.java
+++ src/main/java/com/couchbase/client/core/endpoint/AbstractGenericHandler.java
@@ -248,20 +248,25 @@ public abstract class AbstractGenericHandler<RESPONSE, ENCODED, REQUEST extends 
         if (currentRequest != null && currentOpTime >= 0 && env() != null
             && env().networkLatencyMetricsCollector().isEnabled()) {
 
-            Class<? extends CouchbaseRequest> requestClass = currentRequest.getClass();
-            String simpleName = classNameCache.get(requestClass);
-            if (simpleName == null) {
-                simpleName = requestClass.getSimpleName();
-                classNameCache.put(requestClass, simpleName);
-            }
+            try {
+                Class<? extends CouchbaseRequest> requestClass = currentRequest.getClass();
+                String simpleName = classNameCache.get(requestClass);
+                if (simpleName == null) {
+                    simpleName = requestClass.getSimpleName();
+                    classNameCache.put(requestClass, simpleName);
+                }
 
-            NetworkLatencyMetricsIdentifier identifier = new NetworkLatencyMetricsIdentifier(
-                    remoteHostname,
-                    serviceType().toString(),
-                    simpleName,
-                    response.status().toString()
-            );
-            env().networkLatencyMetricsCollector().record(identifier, currentOpTime);
+                NetworkLatencyMetricsIdentifier identifier = new NetworkLatencyMetricsIdentifier(
+                        remoteHostname,
+                        serviceType().toString(),
+                        simpleName,
+                        response.status().toString()
+                );
+                env().networkLatencyMetricsCollector().record(identifier, currentOpTime);
+            } catch (Throwable e) {
+                LOGGER.warn("Could not collect latency metric for request + "
+                    + currentRequest + "(" + currentOpTime + ")", e);
+            }
         }
     }
 
