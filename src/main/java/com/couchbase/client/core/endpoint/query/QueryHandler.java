6d0478eea609dd325ca5493d65441f0c3559bef4
Simon Baslé <simon.basle@gmail.com>
2016-05-31 14:18:22 +0200

    JVMCBC-338: Fix parsing of results only, no errors/metrics
    Motivation:
    -----------
    The previous change was incomplete in the sense that a response with
    only results followed by status would still fail and timeout.
    
    Modifications:
    --------------
    The QueryHandler has been modified to work with a response where the
    metrics section is omitted, and there's only results (no errors/warn).
    
    This involves detecting end of a results section despite not having a
    new { after said section => rely also on lastChunk.
    
    Added a "success 1" test without metrics.
    Modified 1 test so that it uses only one DefaultLastHttpContent instead
    of several in a row (as that would trigger lastChunk detection).
    
    Results:
    --------
    Response without metrics is now supported by the QueryHandler.
    
    Change-Id: I48dadf20bb22b13e9ae2a0b789b3265515744aab
    Reviewed-on: http://review.couchbase.org/64600
    Tested-by: Simon Baslé <simon@couchbase.com>
    Reviewed-by: Michael Nitschinger <michael@nitschinger.at>

--- src/main/java/com/couchbase/client/core/endpoint/query/QueryHandler.java
+++ src/main/java/com/couchbase/client/core/endpoint/query/QueryHandler.java
@@ -539,7 +539,7 @@ public class QueryHandler extends AbstractGenericHandler<HttpObject, HttpRequest
     private void parseQueryRows(boolean lastChunk) {
         while (true) {
             int openBracketPos = findNextChar(responseContent, '{');
-            if (isEmptySection(openBracketPos)) {
+            if (isEmptySection(openBracketPos) || (lastChunk && openBracketPos < 0)) {
                 sectionDone();
                 queryParsingState = transitionToNextToken(lastChunk);
                 break;
@@ -596,7 +596,7 @@ public class QueryHandler extends AbstractGenericHandler<HttpObject, HttpRequest
     private void parseQueryError(boolean lastChunk) {
         while (true) {
             int openBracketPos = findNextChar(responseContent, '{');
-            if (isEmptySection(openBracketPos) || (openBracketPos < 0 && lastChunk)) {
+            if (isEmptySection(openBracketPos) || (lastChunk && openBracketPos < 0)) {
                 sectionDone();
                 queryParsingState = transitionToNextToken(lastChunk); //warnings or status
                 break;
--- src/test/java/com/couchbase/client/core/endpoint/query/QueryHandlerTest.java
+++ src/test/java/com/couchbase/client/core/endpoint/query/QueryHandlerTest.java
@@ -459,13 +459,13 @@ public class QueryHandlerTest {
     public void shouldDecodeNRowResponseChunked() throws Exception {
         String response = Resources.read("success_5.json", this.getClass());
         HttpResponse responseHeader = new DefaultHttpResponse(HttpVersion.HTTP_1_1, new HttpResponseStatus(200, "OK"));
-        HttpContent responseChunk1 = new DefaultLastHttpContent(Unpooled.copiedBuffer(response.substring(0, 300),
+        HttpContent responseChunk1 = new DefaultHttpContent(Unpooled.copiedBuffer(response.substring(0, 300),
             CharsetUtil.UTF_8));
-        HttpContent responseChunk2 = new DefaultLastHttpContent(Unpooled.copiedBuffer(response.substring(300, 950),
+        HttpContent responseChunk2 = new DefaultHttpContent(Unpooled.copiedBuffer(response.substring(300, 950),
             CharsetUtil.UTF_8));
-        HttpContent responseChunk3 = new DefaultLastHttpContent(Unpooled.copiedBuffer(response.substring(950, 1345),
+        HttpContent responseChunk3 = new DefaultHttpContent(Unpooled.copiedBuffer(response.substring(950, 1345),
             CharsetUtil.UTF_8));
-        HttpContent responseChunk4 = new DefaultLastHttpContent(Unpooled.copiedBuffer(response.substring(1345, 3000),
+        HttpContent responseChunk4 = new DefaultHttpContent(Unpooled.copiedBuffer(response.substring(1345, 3000),
             CharsetUtil.UTF_8));
         HttpContent responseChunk5 = new DefaultLastHttpContent(Unpooled.copiedBuffer(response.substring(3000),
             CharsetUtil.UTF_8));
@@ -1005,6 +1005,44 @@ public class QueryHandlerTest {
         List<String> expectedItems = Arrays.asList("\"u,s,e,r,t,a,\\\"b,l,e:userBBB\\\\\"");
         assertEquals(1, invokeCounter1.get());
         assertEquals(expectedItems, items);
+    }
+
+    @Test
+    public void shouldDecodeSuccess1NoMetrics() throws Exception {
+        String response = Resources.read("success_1_noMetrics.json", this.getClass());
+        HttpResponse responseHeader = new DefaultHttpResponse(HttpVersion.HTTP_1_1, new HttpResponseStatus(200, "OK"));
+        HttpContent responseChunk = new DefaultLastHttpContent(Unpooled.copiedBuffer(response, CharsetUtil.UTF_8));
+
+        GenericQueryRequest requestMock = mock(GenericQueryRequest.class);
+        queue.add(requestMock);
+        channel.writeInbound(responseHeader, responseChunk);
+        latch.await(1, TimeUnit.SECONDS);
+        assertEquals(1, firedEvents.size());
+        GenericQueryResponse inbound = (GenericQueryResponse) firedEvents.get(0);
+
+        final List<String> items = Collections.synchronizedList(new ArrayList<String>(11));
+        final AtomicInteger invokeCounter1 = new AtomicInteger();
+        assertResponse(inbound, true, ResponseStatus.SUCCESS, "ff226b49-9d4c-415b-8428-263cb080e184", "", "success", "{\"*\":\"*\"}",
+                new Action1<ByteBuf>() {
+                    @Override
+                    public void call(ByteBuf buf) {
+                        String item = buf.toString(CharsetUtil.UTF_8).trim();
+                        System.out.println("item #" + invokeCounter1.incrementAndGet() + " = " + item);
+                        items.add(item);
+                        buf.release();
+                    }
+                },
+                new Action1<ByteBuf>() {
+                    @Override
+                    public void call(ByteBuf buf) {
+                        buf.release();
+                        fail("no error expected");
+                    }
+                },
+                null
+        );
+        assertEquals(1, invokeCounter1.get());
+        assertEquals("{\"adHoc_N1qlQuery492841478131758\":{\"item\":\"value\"}}", items.get(0).replaceAll("\\s", ""));
     }
 
     private void shouldDecodeChunkedWithRaw(final int expectedResults, final int expectedErrors, String... chunks) throws Exception {
