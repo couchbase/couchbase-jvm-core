4c66d2e9d3ae058c5fbd920234a9165a506ffbe7
Michael Nitschinger <michael@nitschinger.at>
2016-06-03 07:01:27 +0200

    JVMCBC-340: Fix KeepAlive not being sent anymore.
    Motivation
    ----------
    It was found that KeepAlives are not sent anymore after java-client
    version 2.2.6 and later. Tracking this down, this is a regression
    which has been introduced in the changeset for JVMCBC-300 which is an
    optimization to send a void promise to save allocations.
    
    As part of that change it was needed to change the netty idle state
    handler from checking either read or write ("all") idle states to
    only use "read" since write can't be used anymore on a void promise.
    
    Modifications
    -------------
    The bug shows up because the code in the AbstractGenericHandler listens
    for the "ALL_IDLE" token event, but netty is now emitting the "READ_IDLE"
    event instead. The unit tests did not cover this since they also emit the
    "ALL_IDLE" token to mirror the behavior of netty.
    
    This changeset fixes the token to "READ_IDLE", adapts the unit tests and
    adds an integration test to make sure the keepalives are really sent.
    
    Result
    ------
    The KeepAlives are now sent again.
    
    Change-Id: I8d259fd2604f88f751bd38a449aca9acc776964e
    Reviewed-on: http://review.couchbase.org/64703
    Reviewed-by: Simon Baslé <simon@couchbase.com>
    Tested-by: Michael Nitschinger <michael@nitschinger.at>

---
+++ src/integration/java/com/couchbase/client/core/cluster/KeepAliveMessageTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.couchbase.client.core.cluster;
+
+import com.couchbase.client.core.event.CouchbaseEvent;
+import com.couchbase.client.core.event.metrics.LatencyMetric;
+import com.couchbase.client.core.event.metrics.NetworkLatencyMetricsEvent;
+import com.couchbase.client.core.metrics.NetworkLatencyMetricsIdentifier;
+import com.couchbase.client.core.util.ClusterDependentTest;
+import org.junit.Test;
+import rx.Observable;
+import rx.functions.Func1;
+import rx.observers.TestSubscriber;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Verifies that KeepAlives are really sent across idle channels
+ *
+ * @author Michael Nitschinger
+ * @since 1.3.0
+ */
+public class KeepAliveMessageTest extends ClusterDependentTest {
+
+    @Test
+    public void shouldSendKeepAlives() throws Exception {
+        Observable<CouchbaseEvent> eventBus = env().eventBus().get();
+        TestSubscriber<CouchbaseEvent> eventSubscriber = new TestSubscriber<CouchbaseEvent>();
+        eventBus
+                .filter(new Func1<CouchbaseEvent, Boolean>() {
+                    @Override
+                    public Boolean call(CouchbaseEvent event) {
+                        return event instanceof NetworkLatencyMetricsEvent;
+                    }
+                }).subscribe(eventSubscriber);
+
+        Thread.sleep(ClusterDependentTest.KEEPALIVE_INTERVAL + 1000);
+
+        env().networkLatencyMetricsCollector().triggerEmit();
+
+        Thread.sleep(100);
+
+        List<CouchbaseEvent> events = eventSubscriber.getOnNextEvents();
+        assertEquals(1, events.size());
+        NetworkLatencyMetricsEvent event = (NetworkLatencyMetricsEvent) events.get(0);
+
+        int keepalivesFound = 0;
+        for (Map.Entry<NetworkLatencyMetricsIdentifier, LatencyMetric> metric : event.latencies().entrySet()) {
+            System.err.println(metric.getKey());
+            if (metric.getKey().request().equals("KeepAliveRequest")) {
+                keepalivesFound++;
+            }
+        }
+        assertTrue(keepalivesFound > 0);
+    }
+}
--- src/test/java/com/couchbase/client/core/endpoint/query/QueryHandlerTest.java
+++ src/test/java/com/couchbase/client/core/endpoint/query/QueryHandlerTest.java
@@ -775,7 +775,7 @@ public class QueryHandlerTest {
         EmbeddedChannel channel = new EmbeddedChannel(testHandler);
 
         //test idle event triggers a query keepAlive request and hook is called
-        testHandler.userEventTriggered(ctxRef.get(), IdleStateEvent.FIRST_ALL_IDLE_STATE_EVENT);
+        testHandler.userEventTriggered(ctxRef.get(), IdleStateEvent.FIRST_READER_IDLE_STATE_EVENT);
 
         assertEquals(1, keepAliveEventCounter.get());
         assertTrue(queue.peek() instanceof QueryHandler.KeepAliveRequest);
