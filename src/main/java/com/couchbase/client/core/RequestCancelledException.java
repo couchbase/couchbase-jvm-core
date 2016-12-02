9994a52a95e6c799b74eda4aefbd7cd42867c3d8
Subhashni Balakrishnan <b.subhashni@gmail.com>
2016-12-02 11:53:47 -0800

    JVMCBC-314 Javadoc for RequestCancelledException
    Change-Id: I6e6eabd0d4a87c0c29fa3e4e8eb9d0c167dd0c99
    Reviewed-on: http://review.couchbase.org/70577
    Reviewed-by: Michael Nitschinger <michael@nitschinger.at>
    Tested-by: Subhashni Balakrishnan <b.subhashni@gmail.com>

--- src/main/java/com/couchbase/client/core/RequestCancelledException.java
+++ src/main/java/com/couchbase/client/core/RequestCancelledException.java
@@ -15,6 +15,24 @@
  */
 package com.couchbase.client.core;
 
+/**
+ * This exception is thrown when there is a problem in the underlying network I/O. Non-idempotent requests
+ * should be retried carefully while retrying idempotent ones is more straight forward.
+ * Events that trigger this exception are
+ *
+ *  - The underlying connection to a particular endpoint on the server is closed after the request is dispatched.
+ *  So the outstanding requests in flight are cancelled out. It is not definitive if the operation indeed
+ *  succeeded on the server and the socket error happened while a response was on its way out.
+ *
+ *  - The request cannot be dispatched to the server's endpoint due to various reasons like downed node,
+ *  request queue limit, particular service failure on the node,... If the failure was due to request queue
+ *  limit, try increasing the value on the system property "com.couchbase.sentRequestQueueLimit". The SDK will
+ *  automatically retry the request based on the retry strategy - Best Effort will retry until maxRequestLifetime
+ *  and Fail Fast will cancel the request immediately.
+ *
+ * @author Michael Nitschinger
+ * @author Subhashni Balakrishnan
+ */
 public class RequestCancelledException extends CouchbaseException {
 
     public RequestCancelledException() {
