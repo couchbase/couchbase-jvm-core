5fd3e091e4930ee678ecd38622a9a9b3a5a4f818
Michael Nitschinger <michael@nitschinger.at>
2017-07-12 14:23:31 +0200

    JVMCBC-439: Fix prepared statement dispatching with node hostname
    Motivation
    ----------
    This is a regression. When the NetworkAddress has been introduced
    this codepath in the QueryLocator has not been modified and now
    the equality comparison always fails between InetAddr and NetworkAddress
    - thanks java for not failng to compile ;-)
    
    Modifications
    -------------
    Convert to a NetworkAddress on the fly and as a result handle the
    equality comparison properly, fixing the regression and allowing the
    Prelocated Requests to dispatch properly.
    
    Result
    ------
    Regression fixed.
    
    Change-Id: I3007e25648ee34c0affd0beccab13d1a8c0522df
    Reviewed-on: http://review.couchbase.org/80574
    Reviewed-by: Michael Nitschinger <michael@nitschinger.at>
    Tested-by: Michael Nitschinger <michael@nitschinger.at>

--- src/main/java/com/couchbase/client/core/node/locate/QueryLocator.java
+++ src/main/java/com/couchbase/client/core/node/locate/QueryLocator.java
@@ -26,6 +26,7 @@ import com.couchbase.client.core.node.Node;
 import com.couchbase.client.core.retry.RetryHelper;
 import com.couchbase.client.core.service.ServiceType;
 import com.couchbase.client.core.utils.MathUtils;
+import com.couchbase.client.core.utils.NetworkAddress;
 import com.lmax.disruptor.RingBuffer;
 
 import java.net.InetAddress;
@@ -52,7 +53,7 @@ public class QueryLocator implements Locator {
         }
 
         if (request instanceof PrelocatedRequest && ((PrelocatedRequest) request).sendTo() != null) {
-            InetAddress target = ((PrelocatedRequest) request).sendTo();
+            NetworkAddress target = NetworkAddress.create(((PrelocatedRequest) request).sendTo().getHostAddress());
             for (Node node : nodes) {
                 if (node.hostname().equals(target)) {
                     node.send(request);
