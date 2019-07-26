ef83d3b882d0cd87de158b932e7cfae92429d8bc
David Kelly <davidmichaelkelly@gmail.com>
2019-07-26 15:19:36 -0700

    JVMCBC-574 NetworkAddress issues with ipv6
    Motivation
    =========
    We need to test NetworkAddress creation with ipv6 strings.
    
    Modification
    ============
    Seems my hosts file didn't have ::1 mapped to localhost.  But that
    is standard practice - and the integration test machines certainly
    do.  So - minor tweak to one of the tests for ::1
    
    Change-Id: I70f447e2e57c135d8e86d21ce1c1eda0c9d9ce70
    Reviewed-on: http://review.couchbase.org/112557
    Reviewed-by: Graham Pople <grahampople@gmail.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com>

--- src/test/java/com/couchbase/client/core/utils/NetworkAddressTest.java
+++ src/test/java/com/couchbase/client/core/utils/NetworkAddressTest.java
@@ -90,9 +90,9 @@ public class NetworkAddressTest {
     public void shouldCreateFromIpv6Address() {
         NetworkAddress na = new NetworkAddress("[::1]", false);
         assertEquals("::1", na.address());
-        assertEquals("0:0:0:0:0:0:0:1", na.hostname());
-        assertEquals("0:0:0:0:0:0:0:1", na.nameOrAddress());
-        assertEquals("::1/0:0:0:0:0:0:0:1", na.nameAndAddress());
+        assertEquals("localhost", na.hostname());
+        assertEquals("localhost", na.nameOrAddress());
+        assertEquals("::1/localhost", na.nameAndAddress());
 
         NetworkAddress na2 = new NetworkAddress("[2001:DB4:BAD:A55::5]", false);
         assertEquals("2001:DB4:BAD:A55::5".toLowerCase(), na2.address());
