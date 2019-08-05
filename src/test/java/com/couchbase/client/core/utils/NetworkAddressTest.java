b4684082fa5f8ce322de13f9f988b483dbce77ad
David Kelly <davidmichaelkelly@gmail.com>
2019-08-05 08:36:42 -0700

    JVMCBC-574 NetworkAddress issues with ipv6
    Motivation
    ==========
    We need to test NetworkAddress creation with ipv6 strings.
    
    Modification
    ============
    Some test machines are inconsistent in mapping [::1] as localhost
    and one of our tests assumes that.  Lets remove that, but keep the
    other ipv6 address test.
    
    Change-Id: Ia44d4ff15657501cd02163ceec5ce7cfada0cc0f
    Reviewed-on: http://review.couchbase.org/112925
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: David Nault <david.nault@couchbase.com>

--- src/test/java/com/couchbase/client/core/utils/NetworkAddressTest.java
+++ src/test/java/com/couchbase/client/core/utils/NetworkAddressTest.java
@@ -88,12 +88,6 @@ public class NetworkAddressTest {
 
     @Test
     public void shouldCreateFromIpv6Address() {
-        NetworkAddress na = new NetworkAddress("[::1]", false);
-        assertEquals("::1", na.address());
-        assertEquals("localhost", na.hostname());
-        assertEquals("localhost", na.nameOrAddress());
-        assertEquals("::1/localhost", na.nameAndAddress());
-
         NetworkAddress na2 = new NetworkAddress("[2001:DB4:BAD:A55::5]", false);
         assertEquals("2001:DB4:BAD:A55::5".toLowerCase(), na2.address());
         assertEquals("2001:DB4:BAD:A55:0:0:0:5".toLowerCase(), na2.hostname());
